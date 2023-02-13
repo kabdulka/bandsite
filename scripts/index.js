8f8aac9582883a37da0fceff6fc37def3d56a270
kenan abdulkarim <kenabdulka@gmail.com>
2023-02-13 11:04:06 -0800

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+// let currentKey = "firsName";
+
+
+
+// let obj = {
+//     // Key      value
+//     firstName: "person",
+//     lastName: "Doe"
+
+// };
+
+// Both are the same if you want to access an object's value using its key
+// method 1
+// obj.firstName = "Kenan";
+
+// method 2
+// console.log(obj["firstName"]);
+
+
+let comments = [];
+
+function displayComment(element) {
+
+}
+
+
+
+// You must have an array in JavaScript with 3 default comment objects to start. Comments must have a name, a timestamp, and the comment text.
+// You must have a function called displayComment() that takes in one comment object as a parameter and displays it on the page using JavaScript DOM manipulation.
+// All dynamic HTML should be added to DOM via DOM Methods for individual elements. Avoid bulk assigning stringified HTML using innerHTML
+// You must use an HTML Form with the following functionality:
+// That submits using the addEventListener
+// Prevents the page from reloading when submitting a new comment
+// Constructs a new comment object
+// Pushes a new comment object to an array of comments
+// Clears all comments from the page
+// Re-renders to the page all comments from the comment array
+// Clears the input fields after submitting a new commen
