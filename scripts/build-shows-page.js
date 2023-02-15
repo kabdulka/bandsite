cfd12b25af66ab499521834286fae5cd6df1b1b7
kenan abdulkarim <kenabdulka@gmail.com>
2023-02-14 22:32:57 -0800

    add show title in desktop breakpoint

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -47,57 +47,56 @@ const showsList = document.querySelector(".shows__list");
 // TODO Make this into a function called display...
 shows.forEach(show => {
 
-    // createShowItem(showsList, show.date, show.venue, show.location);
-
     // create an li element
     const listItem = document.createElement("li");
     // give it a class name of shows__item
     listItem.classList.add("shows__item");
     showsList.appendChild(listItem);
 
-    const dateItem = document.createElement("div");
-    dateItem.classList.add("shows__date");
-    listItem.appendChild(dateItem);
-    // createDateContent(dateItem, dateValue);
-    const dateTitle = document.createElement("p");
-   
-    dateTitle.classList.add("shows__date-title");
-    dateTitle.innerText = "DATE";
-    const date = document.createElement("p");
+    const showsInfoDiv = document.createElement("div");
+    showsInfoDiv.classList.add("shows__info");
+    listItem.appendChild(showsInfoDiv);
+
+        const dateItem = document.createElement("div");
+        dateItem.classList.add("shows__date");
+        showsInfoDiv.appendChild(dateItem);
+        const dateTitle = document.createElement("p");
     
-    date.classList.add("shows__date-value");
-    date.innerText = show.date;
-    dateItem.appendChild(dateTitle);
-    dateItem.appendChild(date);
+        dateTitle.classList.add("shows__date-title");
+        dateTitle.innerText = "DATE";
+        const date = document.createElement("p");
+        
+        date.classList.add("shows__date-value");
+        date.innerText = show.date;
+        dateItem.appendChild(dateTitle);
+        dateItem.appendChild(date);
 
-    const venueItem = document.createElement("div");
-    venueItem.classList.add("shows__venue");
-    listItem.appendChild(venueItem);
-    // createVenueContent(venueItem, venueValue);
+        const venueItem = document.createElement("div");
+        venueItem.classList.add("shows__venue");
+        showsInfoDiv.appendChild(venueItem);
 
-    const venueTitle = document.createElement("p");
-    venueTitle.classList.add("shows__venue-title");
-    venueTitle.innerText = "VENUE";
-    
-    const venue = document.createElement("p");
-    venue.classList.add("shows__venue-value");
-    venue.innerText = show.venue;
+        const venueTitle = document.createElement("p");
+        venueTitle.classList.add("shows__venue-title");
+        venueTitle.innerText = "VENUE";
+        
+        const venue = document.createElement("p");
+        venue.classList.add("shows__venue-value");
+        venue.innerText = show.venue;
 
-    venueItem.appendChild(venueTitle);
-    venueItem.appendChild(venue);
+        venueItem.appendChild(venueTitle);
+        venueItem.appendChild(venue);
 
-    const locationItem = document.createElement("div");
-    locationItem.classList.add("shows__location");
-    listItem.appendChild(locationItem);
-    // createLocationContent(locationItem, locationValue);
+        const locationItem = document.createElement("div");
+        locationItem.classList.add("shows__location");
+        showsInfoDiv.appendChild(locationItem);
 
-    const locationTitle = document.createElement("p");
-    locationTitle.classList.add("shows__location-title");
-    locationTitle.innerText = "LOCATION";
-    
-    const location = document.createElement("p");
-    location.classList.add("shows__location-value");
-    location.innerText = show.location;
+        const locationTitle = document.createElement("p");
+        locationTitle.classList.add("shows__location-title");
+        locationTitle.innerText = "LOCATION";
+        
+        const location = document.createElement("p");
+        location.classList.add("shows__location-value");
+        location.innerText = show.location;
 
     locationItem.appendChild(locationTitle);
     locationItem.appendChild(location);
@@ -114,91 +113,91 @@ shows.forEach(show => {
 
 });
 
-function createShowItem (parentElement, dateValue, venueValue, locationValue) {
-    // create an li element
-    const listItem = document.createElement("li");
-    // give it a class name of shows__item
-    listItem.classList.add("shows__item");
-    //make it a chile of showList (ul)
-    parentElement.appendChild(listItem);
-    createDateContainer(listItem, dateValue);
-    createVenueContainer(listItem, venueValue);
-    createLocationContainer(listItem, locationValue);
-    createButton(listItem);
-}
+// function createShowItem (parentElement, dateValue, venueValue, locationValue) {
+//     // create an li element
+//     const listItem = document.createElement("li");
+//     // give it a class name of shows__item
+//     listItem.classList.add("shows__item");
+//     //make it a chile of showList (ul)
+//     parentElement.appendChild(listItem);
+//     createDateContainer(listItem, dateValue);
+//     createVenueContainer(listItem, venueValue);
+//     createLocationContainer(listItem, locationValue);
+//     createButton(listItem);
+// }
 
 // create div container with a class of shows__date
-function createDateContainer (parentElement, dateValue) {
-    const dateItem = document.createElement("div");
-    dateItem.classList.add("shows__date");
-    parentElement.appendChild(dateItem);
-    createDateContent(dateItem, dateValue);
-}
+// function createDateContainer (parentElement, dateValue) {
+//     const dateItem = document.createElement("div");
+//     dateItem.classList.add("shows__date");
+//     parentElement.appendChild(dateItem);
+//     createDateContent(dateItem, dateValue);
+// }
 
-function createDateContent (parentElement, dateValue) {
-    const dateTitle = document.createElement("p");
+// function createDateContent (parentElement, dateValue) {
+//     const dateTitle = document.createElement("p");
    
-    dateTitle.classList.add("shows__date-dateTitle");
-    dateTitle.innerText = "DATE";
-    const date = document.createElement("p");
+//     dateTitle.classList.add("shows__date-dateTitle");
+//     dateTitle.innerText = "DATE";
+//     const date = document.createElement("p");
     
-    date.classList.add("shows__date-value");
-    date.innerText = dateValue;
-    parentElement.appendChild(dateTitle);
-    parentElement.appendChild(date);
+//     date.classList.add("shows__date-value");
+//     date.innerText = dateValue;
+//     parentElement.appendChild(dateTitle);
+//     parentElement.appendChild(date);
 
-}//end create date function
+// }//end create date function
 
-function createVenueContainer (parentElement, venueValue) {
-    const venueItem = document.createElement("div");
-    venueItem.classList.add("shows__venue");
-    parentElement.appendChild(venueItem);
-    createVenueContent(venueItem, venueValue);
-}
+// function createVenueContainer (parentElement, venueValue) {
+//     const venueItem = document.createElement("div");
+//     venueItem.classList.add("shows__venue");
+//     parentElement.appendChild(venueItem);
+//     createVenueContent(venueItem, venueValue);
+// }
 
-function createVenueContent (parentElement, venueValue) {
-    const dateTitle = document.createElement("p");
-    dateTitle.classList.add("shows__venue-dateTitle");
-    dateTitle.innerText = "VENUE";
+// function createVenueContent (parentElement, venueValue) {
+//     const dateTitle = document.createElement("p");
+//     dateTitle.classList.add("shows__venue-dateTitle");
+//     dateTitle.innerText = "VENUE";
     
-    const venue = document.createElement("p");
-    venue.classList.add("shows__venue-value");
-    venue.innerText = venueValue;
+//     const venue = document.createElement("p");
+//     venue.classList.add("shows__venue-value");
+//     venue.innerText = venueValue;
 
-    parentElement.appendChild(dateTitle);
-    parentElement.appendChild(venue);
-}
+//     parentElement.appendChild(dateTitle);
+//     parentElement.appendChild(venue);
+// }
 
-function createLocationContainer (parentElement, locationValue) {
-    const locationItem = document.createElement("div");
-    locationItem.classList.add("shows__location");
-    parentElement.appendChild(locationItem);
-    createLocationContent(locationItem, locationValue);
-}
+// function createLocationContainer (parentElement, locationValue) {
+//     const locationItem = document.createElement("div");
+//     locationItem.classList.add("shows__location");
+//     parentElement.appendChild(locationItem);
+//     createLocationContent(locationItem, locationValue);
+// }
 
-function createLocationContent (parentElement, locationValue) {
-    const dateTitle = document.createElement("p");
-    dateTitle.classList.add("shows__location-dateTitle");
-    dateTitle.innerText = "LOCATION";
+// function createLocationContent (parentElement, locationValue) {
+//     const dateTitle = document.createElement("p");
+//     dateTitle.classList.add("shows__location-dateTitle");
+//     dateTitle.innerText = "LOCATION";
     
-    const location = document.createElement("p");
-    location.classList.add("shows__location-value");
-    location.innerText = locationValue;
+//     const location = document.createElement("p");
+//     location.classList.add("shows__location-value");
+//     location.innerText = locationValue;
 
-    parentElement.appendChild(dateTitle);
-    parentElement.appendChild(location);
-}
+//     parentElement.appendChild(dateTitle);
+//     parentElement.appendChild(location);
+// }
 
-function createButton (parentElement) {
-    const btnContainer = document.createElement("div");
-    btnContainer.classList.add("shows__btn");
-    parentElement.appendChild(btnContainer);
+// function createButton (parentElement) {
+//     const btnContainer = document.createElement("div");
+//     btnContainer.classList.add("shows__btn");
+//     parentElement.appendChild(btnContainer);
 
-    const btn = document.createElement("a");
-    btn.classList.add("btn");
-    btn.innerText = "BUY TICKET";
-    btnContainer.appendChild(btn);
-}
+//     const btn = document.createElement("a");
+//     btn.classList.add("btn");
+//     btn.innerText = "BUY TICKET";
+//     btnContainer.appendChild(btn);
+// }
 
 
 // let tabletBreakpoint = window.matchMedia("(max-width: 700px)");
@@ -221,24 +220,3 @@ function createButton (parentElement) {
 // const dateItem = document.querySelectorAll(".shows__date-dateTitle");
 // dateItem[0].style.display = "none";
 
-
-
-
-
-{/* <div class="shows__date">
-<p class="shows__date-dateTitle"> DATE </p>
-<p class="shows__date-value"> Mon Sept 06 2021 </p>
-</div>
-
-<div class="shows__venue">
-<p class="shows__venue-dateTitle"> VENUE </p>
-<p class="shows__venue-value"> Ronald Lane </p>
-</div>
-
-<div class="shows__location">
-<p class="shows__location-dateTitle"> LOCATION </p>
-<p class="shows__location-value"> San Francisco, CA </p>
-</div>
-<div class="shows__btn">
-<a class="btn" href="#"> BUY TICKET </a>
-</div> */}
