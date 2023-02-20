af6487637487b947e75bfe0f01a53a5a155af70a
kenan abdulkarim <kenabdulka@gmail.com>
2023-02-19 21:23:19 -0800

    add comment feature that only displays the three most recent comments

--- scripts/index.js
+++ scripts/index.js
@@ -6,18 +6,21 @@ let comments = [
         name: "Connor Walton",
         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
         date: "02/17/2021",
+        timestanp: 1613537398000
     },
 
     {
         name: "Emilie Beach",
         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-        date: "01/09/2021"
+        date: "01/09/2021",
+        timestanp: 1610167798000
     },
 
     {
         name: "Miles Acosta",
         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-        date: "12/20/2020"
+        date: "12/20/2020",
+        timestanp: 1639959847000
     }
 
 ];
@@ -112,28 +115,112 @@ commentForm.addEventListener("submit", (event) => {
     // console.log(event.target.comment.value);
     commentObj = {};
 
+    let error = false;
+
     let isNameProvided = event.target.name.value === "";
     let isNCommentProvided = event.target.comment.value === "";
 
+    // check the state of the placeholder. It will tell us whether or not the last submit request had produced an error
+    if (event.target.name.placeholder === "Please enter a name" || event.target.comment.placeholder === "Please enter a comment") {
+        error = true;
+        console.log('error is here now');
+        // add the error class 
+        event.target.name.classList.add("form__comment-name--error");
+    }
+
+    if (error === false ) {
+        event.target.name.placeholder = "Enter your name";
+        // event.target.name.classList.toggle("form__comment-name--error");
+        event.target.comment.placeholder = "Add a new comment";
+        event.target.name.classList.remove("form__comment-name--error");
+    }
+   
     console.log("name provided " + isNameProvided);
     console.log("comment provided " + isNCommentProvided);
 
     if (!isNameProvided && !isNCommentProvided) {
-        // comments.push(event.targer.name.value);
         commentObj.name = event.target.name.value;
         commentObj.comment = event.target.comment.value;
-        commentObj.date = Date.now();
+        const postedDate = new Date(Date.now());
+        console.log(`timestamp is ${postedDate}`);
+        postedDate.toLocaleDateString('en-US');
+
+        // commentObj.date = Date.now();
+        // console.log(`${postedDate.getMonth()}/${postedDate.getDate()}/${postedDate.getFullYear()}`);
+        let day = postedDate.getDate() + "";
+        let month = postedDate.getMonth() + "";
+        let year = postedDate.getFullYear() + "";
+        let hoursInSeconds = postedDate.getHours()*3600;
+        let minutesInSeconds = postedDate.getMinutes()*60;
+        let seconds = postedDate.getSeconds();
+        let totalTimeInSec = hoursInSeconds + minutesInSeconds + seconds;
+   
+        // console.log(date.getDate() + '/' +  date.getMonth() + '/' + date.getFullYear());
+        // console.log(typeof(postedDate.getMonth()));
+        // console.log((postedDate.getMonth()/10 < 1));
+        if (postedDate.getMonth()/10 < 1 ) {
+            // the day is a single number
+            //TODO
+            console.log("month less than 10");
+            month = "0" + month;
+            console.log(month);
+        }
+        if (postedDate.getDate()/10 < 1 ) {
+            // the day is a single number
+            //TODO
+            console.log("month less than 10");
+            day = "0" + day;
+            console.log(day);
+        }
+
+        commentObj.date = `${month}/${day}/${year}`;
+        commentObj.timestanp = Date.parse(commentObj.date) + totalTimeInSec;
+        console.log(`Total timestamp is ${commentObj.timestanp}`);
+        // console.log(commentObj.date.getTime());
+        console.log("hererere")
+        console.log(Date.parse(commentObj.date));
+        // const date = new Date(1666632563517);
+        // console.log(date.toLocaleDateString('en-US'));
+
+        event.target.name.placeholder = "Please enter your name";
+        event.target.comment.placeholder = "Add a new comment";
+        error = false;
+        // convert date object to a string
+        // commentObj.
         comments.push(commentObj);
+        // sort the array here
+
+
+        // sorts the array in decending order
+        // another way to solve this problem is to only display the last 3 elements of an array
+
+        comments.sort ( (a,b) => {
+            // return Date.parse(b.date)-Date.parse(a.date);
+            return b.timestanp - a.timestanp;
+        }) 
+
+        console.log(comments);
+
+        // comments.forEach ((e) => {
+        //     console.log(e.name);
+        // })
+
+        // convert 
     } else {
         // display error
-        console.log("errorrrr")
-        event.target.value = "Please enter a name";
-        event.target.value = "Please enter a comment";
-        console.log();
+        console.log("errorrrr");
+        // event.target.name.placeholder.style.color = "red";
+        event.target.name.placeholder = "Please enter a name";
+        event.target.name.classList.add("form__comment-name--error");
+        event.target.comment.placeholder = "Please enter a comment";
+        event.target.name.classList.add("form__comment-text--error");
+
+        // event.target.name.style
+        error = true;
     }
 
-    console.log(commentObj.name + " " + commentObj.comment + " " + commentObj.date);
 
+    console.log(commentObj.name + " " + commentObj.comment + " " + commentObj.date);
 
     // console.log(comments[comments.length-1].name);
 
@@ -147,17 +234,25 @@ commentForm.addEventListener("submit", (event) => {
 
     commentForm.reset();
 
-    comments.forEach( (comment) => {
-        displayComment(comment);
-    });
+    // comments.forEach( (comment) => {
+    //     displayComment(comment);
+    // });
+    for (let i = 0; i<3; i++) {
+        displayComment(comments[i]);
+        console.log(Date.parse(comments[i].date));
+    }
     // const nameValue = document.querySelector(".form__comment-name");
     // console.log(nameValue.value);
 });
 
 
-comments.forEach( (comment) => {
-    displayComment(comment);
-});
+// comments.forEach( (comment) => {
+//     displayComment(comment);
+// });
+
+for (let i = 0; i<3; i++) {
+    displayComment(comments[i]);
+}
 
 
 
