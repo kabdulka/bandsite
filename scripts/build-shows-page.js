85301eff4583c8a49834dc7d7ace86efe8549351
kenan abdulkarim <kenabdulka@gmail.com>
2023-02-20 22:39:55 -0800

    add sound cloud and style for mobile tablet and desktop

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -105,11 +105,6 @@ shows.forEach(show => {
     locationItem.appendChild(location);
 
 
-//     <a href="www.google.com" class="shows__btn">
-//     <!-- TODO  make the button clickable anywhere -->
-//     <div class="btn" > BUY TICKETS </div>
-// </a>
-
     const btn = document.createElement("a");
     const btnContainer = document.createElement("div");
     btn.classList.add("shows__btn");
@@ -131,126 +126,14 @@ shows.forEach(show => {
         event.currentTarget.classList.add("shows__item--clicked");
     } );
 
-    //hover events
-
-    // const showItems = document.querySelectorAll(".shows__item");
-    // console.log(showItems)
-    // showItems.forEach (item => {
-    //     item.addEventListener("mouseenter", (event) => {
-    //         event.stopPropagation()
-    //         console.log("hover");
-    //         item.style.color = "red";
-    //     })
-    
-    //     // item.addEventListener("")
-    // }) 
-    
 
 });
-
-
-// const title = document.querySelector(".shows__title");
-// title.addEventListener("mouseover", (e) => {
-//     console.log(e)
-// })
-
-
-
-////// WORK ON clicking show items
-
 
 
 const showItems = document.querySelectorAll(".shows__item");
 
 
-// console.log(showItems);
 
-
-// showItems.addEventListener("click", (event) => {
-//     //remove the class from all items
-//     // then add it
-//     console.log("click click")
-//     showItems.froEach( show => {
-
-//         show.classList.removed("shows__item--clicked");
-//     });
-    
-//     event.target.classList.add("shows__item--clicked");
-
-// })
-
-
-
-// addEventListener("click", (event) => {
-
-// } )
-
-console.log(showItems)
-showItems.forEach (item => {
-    item.addEventListener("mouseover", (event) => {
-        event.stopPropagation()
-        console.log("hover");
-        item.classList.toggle("shows__item--hover");
-        // item.style.color = "red";
-    })
-
-    // item.addEventListener("")
-}) 
-
-showItems.forEach( item => {
-    item.addEventListener("mouseout", () => {
-        console.log("mouse out ")
-        // item.style.color = "black"; 
-        item.classList.toggle("shows__item--hover");
-    })
-})
-
-
-
-
-// let tabletBreakpoint = window.matchMedia("(max-width: 700px)");
-// console.log(tabletBreakpoint);
-// if 
-
-// console.log(window.innerWidth);
-
-// if (window.innerWidth <= 768) {
-//     console.log("true");
-//     const dateItems = document.querySelectorAll(".shows__date-dateTitle");
-//     console.log(dateItems);
-//     dateItems.forEach (item => {
-//         item.style.display = "none";
-//     });
-    
-    // const 
-// }
-
-// const dateItem = document.querySelectorAll(".shows__date-dateTitle");
-// dateItem[0].style.display = "none";
-
-// function replaceUpper (sentence) {
-//     let temp = "";
-//     words = sentence.split(" ");
-//     console.log(words);
-    
-//     for (let i = 0; i<words.length; i++) {
-//         temp = words[i];
-
-//         // firstChar = temp.charAt(0) + "";
-//         // console.log(temp.charAt(0));
-//         temp = temp.charAt(0).toUpperCase() + "" + temp.substring(1, temp.length);
-//         // console.log(words[i].charAt(0));
-//         words[i] = temp;
-
-//     }
-
-//     let result = words.join(" ");
-//     console.log(result);
-//     return result;
-// }
-
-// let ans = replaceUpper("brainstation is fun");
-// console.log(ans);
 
 
 
--- scripts/index.js
+++ scripts/index.js
@@ -25,23 +25,11 @@ let comments = [
 
 ];
 
-// const a = document.querySelector(".at");
-// a.addEventListener('click', () => {
-//     console.log("Clicked a");
-//     alert("Somethin");
-// });
-
-// get the ul element
 const commentList = document.querySelector(".comment__list");
 
-//You must have a function called displayComment() that takes 
-// in one comment object as a parameter and displays it on the page using JavaScript DOM manipulation.
+
 function displayComment(comment) {
 
-    // comments.forEach( (comment) => {
-
-        // console.log(comment.date);
-        //create a list item
         const commentItem = document.createElement("li");
         commentItem.classList.add("comment__item");
         commentList.appendChild(commentItem);
@@ -75,79 +63,32 @@ function displayComment(comment) {
         name.innerText = comment.name;
         date.innerText = comment.date;
         commentText.innerText = comment.comment;
-        
-    // <li class="comment__item">
-    //     <div class="comment__image-container">
-    //         <img class="comment__image" src="" alt="">
-    //     </div>
-    //     <div class="comment__info">
-    //         <div class="comment__info__row-1">
-    //             <p class="comment__name"> Connor Walton </p>
-    //             <p class="comment__date"> 02/17/2021 </p>
-    //         </div>
-    //         <div class="comment__info__row-2">
-    //             <p class="comment__value"> 
-    //                 This is art. This is inexplicable magic expressed in the purest way, 
-    //                 everything that makes up this majestic work deserves reverence. Let us appreciate 
-    //                 this for what it is and what it contains.
-    //             </p>
-    //         </div>
 
-    //     </div>
-    // </li>
-
-} // end function
-
-// displayComment(    {
-//     name: "Miles Acosta",
-//     comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-//     date: "12/20/2020"
-// });
+} // end displayComment function
 
 
-
-// const commentForm = document.querySelector("#form__comment");
 const commentForm = document.querySelector(".form__comment");
 commentForm.addEventListener("submit", (event) => {
 
     event.preventDefault();
-    // commentForm.reset();
-    // console.log(event.target.comment.value);
-    const commentObj = {};
 
-    let error = false;
+    const commentObj = {};
 
     let isNameProvided = event.target.name.value === "";
     let isNCommentProvided = event.target.comment.value === "";
 
-    // check the state of the placeholder. It will tell us whether or not the last submit request had produced an error
-    if (event.target.name.placeholder === "Please enter a name" || event.target.comment.placeholder === "Please enter a comment") {
-        error = true;
-        console.log('error is here now');
-        // add the error class 
-        event.target.name.classList.add("form__comment-name--error");
-    }
 
-    if (error === false ) {
-        event.target.name.placeholder = "Enter your name";
-        // event.target.name.classList.toggle("form__comment-name--error");
-        event.target.comment.placeholder = "Add a new comment";
-        event.target.name.classList.remove("form__comment-name--error");
-    }
-   
-    console.log("name provided " + isNameProvided);
-    console.log("comment provided " + isNCommentProvided);
-
+    // if both fields are NOT empty
     if (!isNameProvided && !isNCommentProvided) {
+        const nameInput = document.querySelector(".form__comment-name");
+        event.target.name.classList.remove("form__comment-name--error");
+        event.target.comment.classList.remove("form__comment-text--error");
         commentObj.name = event.target.name.value;
         commentObj.comment = event.target.comment.value;
         const postedDate = new Date(Date.now());
-        // console.log(`timestamp is ${postedDate}`);
         postedDate.toLocaleDateString('en-US');
-        console.log(`Date dot now is ${Date.now()}`);
         
-        // commentObj.date = Date.now();
-        // console.log(`${postedDate.getMonth()}/${postedDate.getDate()}/${postedDate.getFullYear()}`);
+     
         let day = postedDate.getDate() + "";
         let month = postedDate.getMonth() + "";
         let year = postedDate.getFullYear() + "";
@@ -156,95 +97,53 @@ commentForm.addEventListener("submit", (event) => {
         let seconds = postedDate.getSeconds();
         let totalTimeInSec = hoursInSeconds + minutesInSeconds + seconds;
    
-        // console.log(date.getDate() + '/' +  date.getMonth() + '/' + date.getFullYear());
-        // console.log(typeof(postedDate.getMonth()));
-        // console.log((postedDate.getMonth()/10 < 1));
+
         if (postedDate.getMonth()/10 < 1 ) {
-            // the day is a single number
-            //TODO
-            console.log("month less than 10");
+
             month = "0" + month;
-            console.log(month);
         }
         if (postedDate.getDate()/10 < 1 ) {
-            // the day is a single number
-            //TODO
-            console.log("month less than 10");
             day = "0" + day;
-            console.log(day);
         }
 
         commentObj.date = `${month}/${day}/${year}`;
         commentObj.timestanp = Date.parse(commentObj.date) + totalTimeInSec;
-        // console.log(`Total timestamp is ${commentObj.timestanp}`);
-        // // console.log(commentObj.date.getTime());
-        // console.log("hererere")
-        // console.log(Date.parse(commentObj.date));
-        // const date = new Date(1666632563517);
-        // console.log(date.toLocaleDateString('en-US'));
+
 
         event.target.name.placeholder = "Please enter your name";
         event.target.comment.placeholder = "Add a new comment";
         error = false;
-        // convert date object to a string
-        // commentObj.
+
         comments.push(commentObj);
-        // sort the array here
 
 
         // sorts the array in decending order
         // another way to solve this problem is to only display the last 3 elements of an array
-
         comments.sort ( (a,b) => {
-            // return Date.parse(b.date)-Date.parse(a.date);
             return b.timestanp - a.timestanp;
         }) 
 
-        console.log(comments);
 
-        // convert 
     } else {
-        // display error
-        // event.target.name.placeholder.style.color = "red";
+        // one or both of the fields are empty
         event.target.name.placeholder = "Please enter a name";
         event.target.name.classList.add("form__comment-name--error");
         event.target.comment.placeholder = "Please enter a comment";
-        event.target.name.classList.add("form__comment-text--error");
-
-        // event.target.name.style
-        error = true;
+        event.target.comment.classList.add("form__comment-text--error");
     }
 
-
-    console.log(commentObj.name + " " + commentObj.comment + " " + commentObj.date);
-
-    // console.log(comments[comments.length-1].name);
-
-    // if (event.target.comment.value !== "") {
-    //     commentObj.comment = event.target.comment.value;
-    // }
-
-    // displayComment(commentObj);
 
     clearComments(commentList);
 
-    commentForm.reset();
+    event.target.reset();
 
-    // comments.forEach( (comment) => {
-    //     displayComment(comment);
-    // });
     for (let i = 0; i<3; i++) {
         displayComment(comments[i]);
-        console.log(Date.parse(comments[i].date));
+        // console.log(Date.parse(comments[i].date));
     }
-    // const nameValue = document.querySelector(".form__comment-name");
-    // console.log(nameValue.value);
+
 });
 
-
-// comments.forEach( (comment) => {
-//     displayComment(comment);
-// });
 
 for (let i = 0; i<3; i++) {
     displayComment(comments[i]);
@@ -254,28 +153,43 @@ for (let i = 0; i<3; i++) {
 
 function clearComments (parent) {
 
-    // While there is a child 
     while (parent.firstChild) {
-        console.log("here")
         parent.removeChild(parent.firstChild);
     }
 
 }
 
 
-// let arr = [1,2,3,145,5]
 
-// let sum = 0;
+function timeSince(date) {
+    const seconds = Math.floor((new Date() - date) / 1000);
+    let interval = Math.floor(seconds / 31536000);
+  
+    if (interval >= 1) {
+      return interval + " year" + (interval === 1 ? "" : "s") + " ago";
+    }
+  
+    interval = Math.floor(seconds / 2592000);
+    if (interval >= 1) {
+      return interval + " month" + (interval === 1 ? "" : "s") + " ago";
+    }
+  
+    interval = Math.floor(seconds / 86400);
+    if (interval >= 1) {
+      return interval + " day" + (interval === 1 ? "" : "s") + " ago";
+    }
+  
+    interval = Math.floor(seconds / 3600);
+    if (interval >= 1) {
+      return interval + " hour" + (interval === 1 ? "" : "s") + " ago";
+    }
+  
+    interval = Math.floor(seconds / 60);
+    if (interval >= 1) {
+      return interval + " minute" + (interval === 1 ? "" : "s") + " ago";
+    }
+  
+    return Math.floor(seconds) + " second" + (Math.floor(seconds) === 1 ? "" : "s") + " ago";
+  };
 
-
-// for (let i = 0; i<arr.length; i++) {
-//     sum += arr[i]
-    
-//     if (sum >= 150) {
-//         console.log(i);
-//         break;
-//     }
-// }
-
-// console.log(i)
 
